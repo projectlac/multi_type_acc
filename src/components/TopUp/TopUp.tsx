04b5eb95b5471bf7457d81c07e563a9c2b0f664d
projectlac <dft1711198@gmail.com>
2023-02-08 22:47:35 +0700

    change phone number

--- src/components/TopUp/TopUp.tsx
+++ src/components/TopUp/TopUp.tsx
@@ -479,7 +479,7 @@ function TopUp() {
                       }}
                     >
                       <Typography fontSize={17} mr={2}>
-                        0904448980
+                        0372790362
                         {/* Nạp Momo đang lỗi, vui lòng ib Shop
                         để được nạp tiền */}
                       </Typography>{' '}
@@ -487,7 +487,7 @@ function TopUp() {
                         <IconButton
                           aria-label="copy"
                           onClick={() => {
-                            copySomething('0904448980');
+                            copySomething('0372790362');
                           }}
                         >
                           <ContentCopyIcon
