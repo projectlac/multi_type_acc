9cebc54fd1775fa246a93b3de8c2ff4829fbd31a
Tim Dang <dft1711198@gmail.com>
2023-03-21 13:25:56 +0700

    add noti

--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -1,7 +1,7 @@
 import Table from '@/components/Table/Table';
 import { useAuth } from '@/contexts/AuthGuard';
 import { ProtectGuess } from '@/contexts/ProtectGuess';
-import { Box, Card, Container, styled } from '@mui/material';
+import { Box, Card, Container, styled, Typography } from '@mui/material';
 import { getHistory } from 'api/user';
 import Head from 'next/head';
 import { ReactElement, useEffect, useState } from 'react';
@@ -35,6 +35,26 @@ function Overview() {
           <Box py={3}>
             <Card>
               <Table data={data} />
+            </Card>
+            <Card
+              sx={{
+                mt: 3,
+                px: 3,
+                py: 2
+              }}
+            >
+              <Typography
+                sx={{
+                  fontWeight: 'bold',
+                  a: {
+                    color: '#33d'
+                  }
+                }}
+              >
+                Nếu acc lỗi hay có mail hoặc bất cứ vấn đề gì vui lòng liên hệ
+                qua trang page :{' '}
+                <a href="https://m.me/103780805920496">Ấn vào đây</a>
+              </Typography>
             </Card>
           </Box>
         </Container>
