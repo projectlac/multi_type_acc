0d96838253cadeefea2e285213b0a297b7ae7861
projectlac <dft1711198@gmail.com>
2023-05-10 23:10:48 +0700

    lazy

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,4 +1,4 @@
-import bg from '@/assets/images/genshin-impact-nawpic.jpg';
+import bg from '@/assets/images/genshin-impact-nawpic.webp';
 import Footer from '@/components/Footer';
 import { useAuth } from '@/contexts/AuthGuard';
 import CloseIcon from '@mui/icons-material/Close';
