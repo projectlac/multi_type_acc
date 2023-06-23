a1dc63521e4d6847d9d2f35c19a2296fb5aa354c
projectlac <dft1711198@gmail.com>
2023-06-23 20:21:08 +0700

    Change

--- src/components/Reputation/Reputation.tsx
+++ src/components/Reputation/Reputation.tsx
@@ -20,7 +20,14 @@ import Img14 from '@/assets/images/repu/ecd7999e43069b58c217.jpg';
 import Img15 from '@/assets/images/repu/400383bd9a2742791b36.jpg';
 import Img16 from '@/assets/images/repu/b57d9fef86755e2b0764.jpg';
 
-import { Box, Button, Divider, Grid, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  TextField,
+  Typography
+} from '@mui/material';
 export default function ReputationItem() {
   return (
     <Box>
@@ -59,6 +66,60 @@ export default function ReputationItem() {
         </Typography>
       </Box>
       <Grid container columnSpacing={3} rowSpacing={3}>
+        <Grid item md={2} xs={12}></Grid>
+        <Grid item md={8} xs={12}>
+          <Box
+            sx={{
+              '& div.video': {
+                position: 'relative',
+                height: '0%',
+                borderRadius: '5px',
+                overflow: 'hidden',
+                paddingBottom: { md: `50.3%`, xs: '56.25%' },
+                '& iframe': {
+                  position: 'absolute',
+                  top: '0',
+                  left: '0',
+                  width: '100%',
+                  height: '100%'
+                }
+              }
+            }}
+          >
+            <Box
+              sx={{
+                background: '#fff',
+                padding: '15px',
+                mb: 3,
+                borderRadius: '10px'
+              }}
+            >
+              <Typography
+                sx={{
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                  fontSize: '17px'
+                }}
+              >
+                jonjiban nói gì về shop acc GenshinViet
+              </Typography>
+            </Box>
+
+            <Box className="video">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/cveFouoy2d4"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item md={2} xs={12}></Grid>
+
         <Grid item md={6} xs={12}>
           <Box
             sx={{
@@ -164,13 +225,10 @@ export default function ReputationItem() {
           >
             <Box className="video">
               <iframe
+                src="https://drive.google.com/file/d/1KFduh7r9_9VhV0gBkT2GQIWxyI4EJCx8/preview"
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/-LSlqjNkrLM"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
+                allow="autoplay"
               ></iframe>
             </Box>
             <Box
