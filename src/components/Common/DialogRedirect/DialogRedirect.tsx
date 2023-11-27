c0a4b292c85d96798617d2497a50d70b62c6acbf
Sotatek-TuyenDang2 <tuyen.dang2@sotatek.com>
2023-11-27 17:36:04 +0700

    Change msg

--- src/components/Common/DialogRedirect/DialogRedirect.tsx
+++ src/components/Common/DialogRedirect/DialogRedirect.tsx
@@ -5,10 +5,10 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  Typography
-} from '@mui/material';
-import React from 'react';
-
+  Typography,
+} from "@mui/material";
+import React from "react";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 function DialogRedirect({ open, handleClose }) {
   return (
     <Dialog
@@ -17,22 +17,48 @@ function DialogRedirect({ open, handleClose }) {
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id="alert-dialog-title">{'Thông báo'}</DialogTitle>
+      <DialogTitle id="alert-dialog-title">{"Thông báo"}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          <Typography fontWeight={'bold'}>
-            Hãy ib zalo 0372790362 để biết thêm thông tin chi tiết{' '}
+          <Typography fontWeight={"bold"} color={"red"}>
+            Thông Báo
+          </Typography>
+          <Typography
+            fontWeight={"bold"}
+            color={"red"}
+            sx={{ span: { textTransform: "uppercase" } }}
+          >
+            Web vẫn <span>hoạt động bình thường</span>{" "}
+            vui lòng hãy liên hệ admin:
+          </Typography>
+
+          <Typography
+            fontWeight={"bold"}
+            color={"red"}
+            sx={{ span: { textTransform: "uppercase" } }}
+          >
+            <PlayArrowIcon /> Nếu sử dụng <span>zalo</span>{" "}
+            hãy liên hệ admin ấn vào đây{" "}
             <a href="https://zalo.me/0372790362" target="__blank">
               ấn vào đây
             </a>
           </Typography>
-          <br />{' '}
-          <Typography fontWeight={'bold'}>
-            Hoặc Facebook chủ shop{' '}
+          <Typography
+            fontWeight={"bold"}
+            color={"red"}
+            sx={{ span: { textTransform: "uppercase" } }}
+          >
+            <PlayArrowIcon /> Nếu các bạn sử dụng <span>facebook</span> hãy{" "}
             <a href="https://www.facebook.com/nguyenhung208" target="__blank">
               ấn vào đây
-            </a>
+            </a>{" "}
+            để liên hệ admin
           </Typography>
+
+          Lưu ý : Web vẫn hoạt động bình thường các bạn hãy liên hệ admin để
+          biết thêm thông tin chi tiết
+
+          Cảm ơn các bạn và rất xin lỗi vì sự bất tiện này ❤
         </DialogContentText>
       </DialogContent>
       <DialogActions>
