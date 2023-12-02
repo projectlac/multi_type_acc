f276d5880aea91124169ed92e307f83acf8a0775
projectlac <dft1711198@gmail.com>
2023-12-02 11:25:48 +0700

    Change

--- src/components/Common/DialogRedirect/DialogRedirect.tsx
+++ src/components/Common/DialogRedirect/DialogRedirect.tsx
@@ -16,6 +16,8 @@ function DialogRedirect({ open, handleClose }) {
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      fullWidth
+      maxWidth="sm"
     >
       <DialogTitle id="alert-dialog-title" sx={{ fontSize: 20 }}>
         {'Thông báo'}
@@ -27,8 +29,8 @@ function DialogRedirect({ open, handleClose }) {
             color={'black'}
             sx={{ span: { color: 'red', textTransform: 'uppercase' } }}
           >
-            Web vẫn <span>hoạt động bình thường</span> vui lòng hãy liên hệ
-            admin:
+            Web vẫn <span>hoạt động bình thường</span> vui lòng{' '}
+            <a href="https://genshinviet.com.vn/">ấn vào đây</a>
           </Typography>
           <Typography
             fontWeight={'bold'}
@@ -45,10 +47,15 @@ function DialogRedirect({ open, handleClose }) {
           >
             <PlayArrowIcon />
             <p>
-              Nếu sử dụng <span>zalo</span> hãy liên hệ admin ấn vào đây{' '}
-              <a href="https://zalo.me/0372790362" target="__blank">
-                ấn vào đây
-              </a>
+              Nếu các bạn sử dụng <span>FACEBOOK</span> hãy{' '}
+              <a
+                href="https://m.me/122101845332012058"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ẤN VÀO ĐÂY
+              </a>{' '}
+              để liên hệ admin
             </p>
           </Typography>
           <Typography
@@ -60,16 +67,37 @@ function DialogRedirect({ open, handleClose }) {
               flexWrap: 'wrap',
               alignItems: 'center',
 
+              a: { mx: 0.5, textTransform: 'uppercase' },
+              p: { mb: 0 }
+            }}
+          >
+            <PlayArrowIcon />
+            <p>
+              Nếu page không trả lời hãy{' '}
+              <a href="https://zalo.me/0372790362" target="__blank">
+                <b>ấn vào đây</b>
+              </a>{' '}
+              để liên hệ ADMIN
+            </p>
+          </Typography>
+          <Typography
+            fontWeight={'bold'}
+            color={'red'}
+            sx={{
+              span: { textTransform: 'uppercase' },
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+              p: { mb: 0 },
               a: { mx: 0.5, textTransform: 'uppercase' }
             }}
           >
             <PlayArrowIcon />
-            <p style={{ margin: 0 }}>
-              Nếu các bạn sử dụng <span>facebook</span> hãy{' '}
+            <p>
+              Link facebook chủ shop
               <a href="https://www.facebook.com/nguyenhung208" target="__blank">
                 ấn vào đây
               </a>{' '}
-              để liên hệ admin
             </p>
           </Typography>
           <hr />
@@ -82,8 +110,15 @@ function DialogRedirect({ open, handleClose }) {
               p: { mb: 0 }
             }}
           >
-            Lưu ý: <br /> Web vẫn hoạt động bình thường, <br /> các bạn hãy liên
-            hệ admin để biết thêm thông tin chi tiết <br />
+            Lưu ý: <br /> Web vẫn hoạt động bình thường, <br /> các bạn hãy
+            <a
+              href="https://genshinviet.com.vn/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ẤN VÀO ĐÂY
+            </a>
+            để biết thêm thông tin chi tiết <br />
             Cảm ơn các bạn và rất xin lỗi vì sự bất tiện này ❤
           </Typography>
         </DialogContentText>
