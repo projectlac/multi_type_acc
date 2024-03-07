e89ab8fa44409c18f34cafaba63e681a9d381763
projectlac <dft1711198@gmail.com>
2024-03-07 22:41:55 +0700

    Add role

--- src/content/Management/UsersTable/Action/EditTag.tsx
+++ src/content/Management/UsersTable/Action/EditTag.tsx
@@ -137,7 +137,7 @@ function EditTag({ title, role, id, bonus, type }: IEdit) {
                 setValue(newValue);
                 setAccType(newValue);
               }}
-              options={['VIP', 'REROLL', 'RANDOM', 'NEW', 'CODE']}
+              options={['VIP', 'REROLL', 'RANDOM', 'NEW', 'CODE', 'REROLLVIP']}
               renderInput={(params) => (
                 <MuiTextField
                   {...params}
